3bd761888836d37394c35532613640a4d6a409d3
Joris Durand <jodd@users.noreply.github.com>
2014-11-05 13:12:47 +0100

    create new file for plugin code

---
+++ js/placeholderLabel.js
@@ -0,0 +1,52 @@
+/* -----------------------------------------------------------------------------
+  placeholderLabel
+----------------------------------------------------------------------------- */
+;(function() {
+
+  function manageLabel() {
+    if (this.el.value) {
+      this.$label.addClass('is-displayed');
+    } else {
+      this.$label.removeClass('is-displayed');
+    }
+  }
+
+  // Constructor
+  function placeholderLabel(element, options) {
+    this.el = element;
+    this.$el = $(this.el);
+    this.$label = $('label[for=' + this.el.id + ']').addClass('PlaceholderLabel-label');
+
+    if (!this.$label.length) {
+      if (this.el.placeholder) {
+        this.$label = $('<label class="PlaceholderLabel-label" for="'+ this.el.id +'">' + this.el.placeholder + '</label>')
+          .insertBefore(this.el);
+      } else return;
+    }
+
+    this.el.placeholder = this.el.placeholder || this.$label.text();
+
+    this.init();
+  }
+
+  // Public functions
+  placeholderLabel.prototype = {
+    init: function() {
+      var self = this;
+
+      this.$el.add(this.$label).wrapAll('<div class="PlaceholderLabel-wrapper" />');
+
+      this.$el.on('keyup', manageLabel.bind(this));
+
+      manageLabel.call(this);
+    }
+  };
+
+  // jQuery plugin definition
+  $.fn.placeholderLabel = function(options) {
+    return this.each(function() {
+      new placeholderLabel(this, options);
+    });
+  };
+
+})();
