559139d521010b4dda9fabafa996922954bfdc6e
Joris Durand <jodd@users.noreply.github.com>
2014-11-05 13:07:20 +0100

    cleaning

--- js/main.js
+++ js/main.js
@@ -12,7 +12,7 @@
   }
 
   // Constructor
-  function placeholderLabel(element, options) {console.log('constructor');
+  function placeholderLabel(element, options) {
     this.el = element;
     this.$el = $(this.el);
     this.$label = $('label[for=' + this.el.id + ']').addClass('PlaceholderLabel-label');
@@ -31,7 +31,7 @@
 
   // Public functions
   placeholderLabel.prototype = {
-    init: function() {console.log('init');
+    init: function() {
       var self = this;
 
       this.$el.add(this.$label).wrapAll('<div class="PlaceholderLabel-wrapper" />');
