a2391fab6cfb06524d7875eabb73d8b2ed9bd9cb
Joris Durand <jodd@users.noreply.github.com>
2014-11-17 18:12:47 +0100

    add jquery to bower dependencies + use grunt for sass compile, jshint and other stuff

---
+++ Gruntfile.js
@@ -0,0 +1,105 @@
+/*global module:false*/
+module.exports = function (grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    // Metadata.
+    pkg: grunt.file.readJSON('package.json'),
+    // banner: '/*! <%= pkg.title || pkg.name %> - v<%= pkg.version %> - ' +
+    //   '<%= grunt.template.today("yyyy-mm-dd") %>\n' +
+    //   '<%= pkg.homepage ? "* " + pkg.homepage + "\\n" : "" %>' +
+    //   '* Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author.name %>;' +
+    //   ' Licensed <%= _.pluck(pkg.licenses, "type").join(", ") %> */\n',
+    // Task configuration.
+    // concat: {
+    //   options: {
+    //     banner: '<%= banner %>',
+    //     stripBanners: true
+    //   },
+    //   dist: {
+    //     src: ['lib/<%= pkg.name %>.js'],
+    //     dest: 'dist/<%= pkg.name %>.js'
+    //   }
+    // },
+    // uglify: {
+    //   options: {
+    //     banner: '<%= banner %>'
+    //   },
+    //   dist: {
+    //     src: '<%= concat.dist.dest %>',
+    //     dest: 'dist/<%= pkg.name %>.min.js'
+    //   }
+    // },
+    sass: {
+      dist: {
+        src: 'scss/main.scss',
+        dest: 'css/main.css'
+      }
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        unused: true,
+        boss: true,
+        eqnull: true,
+        browser: true,
+        globals: {
+          jQuery: false,
+          $: false,
+          requirejs: false,
+          require: false,
+          define: false
+        }
+      },
+      gruntfile: {
+        src: 'Gruntfile.js'
+      },
+      scripts: {
+        src: ['js/*.js']
+      },
+      lib_test: {
+        src: ['lib/**/*.js', 'test/**/*.js']
+      }
+    },
+    qunit: {
+      files: ['test/**/*.html']
+    },
+    watch: {
+      sass: {
+        files: '<%= sass.dist.src %>',
+        tasks: ['sass:dist']
+      },
+      gruntfile: {
+        files: '<%= jshint.gruntfile.src %>',
+        tasks: ['jshint:gruntfile']
+      },
+      scripts: {
+        files: '<%= jshint.scripts.src %>',
+        tasks: ['jshint:scripts']
+      },
+      lib_test: {
+        files: '<%= jshint.lib_test.src %>',
+        tasks: ['jshint:lib_test', 'qunit']
+      }
+    }
+  });
+
+  // These plugins provide necessary tasks.
+  // grunt.loadNpmTasks('grunt-contrib-concat');
+  // grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-sass');
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task.
+  grunt.registerTask('default', ['jshint', 'qunit' /*, 'concat', 'uglify'*/ ]);
+
+};
